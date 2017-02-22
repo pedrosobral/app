397118762c28423a902c261cd194e44a1d811a57
Pedro Henrique <pedrosobralxv@gmail.com>
2017-02-22 10:17:39 -0300

    update: save player points

--- src/pages/end-activity/end-activity.ts
+++ src/pages/end-activity/end-activity.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { HomePage } from '../home/home';
+
+import { Player } from '../../providers/player';
+
 @Component({
   selector: 'page-end-activity',
   templateUrl: 'end-activity.html'
 })
 export class EndActivityPage {
+  points = this.navParams.get('points');
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public player: Player, public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.info('params', this.navParams);
+
+    this.player.data.then((data) => {
+      data.points += this.points;
+      this.player.save(data);
+    });
   }
 
+  dismiss() {
+    this.navCtrl.setRoot(HomePage);
+  }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,17 +3,28 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import { QuestionPage } from '../pages';
 
+import { Player } from '../../providers/player';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  points: number;
 
   constructor(
+    public player: Player,
     public navCtrl: NavController,
     public navParams: NavParams) {}
 
   ionViewDidLoad() { }
+
+  ionViewDidEnter() {
+    this.player.data.then((data) => {
+      this.points = data.points;
+      console.info('data', data, this.points);
+    });
+  }
 
   goToQuestions(level) {
     this.navCtrl.push(QuestionPage);
