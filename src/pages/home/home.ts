c0cc6428943bb1e37707cf1adcc6590755fd5d81
Pedro Henrique <pedrosobralxv@gmail.com>
2017-05-06 18:54:50 -0300

    feat(home): add alert questions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { QuestionPage } from '../pages';
 
@@ -15,19 +15,34 @@ export class HomePage {
   constructor(
     public player: Player,
     public navCtrl: NavController,
-    public navParams: NavParams) {}
+    public navParams: NavParams,
+    public alertCtrl: AlertController
+  ) {}
 
   ionViewDidLoad() { }
 
   ionViewDidEnter() {
     this.player.data.then((data) => {
       this.points = data.points;
-      console.info('data', data, this.points);
     });
   }
 
-  goToQuestions(level) {
-    this.navCtrl.push(QuestionPage);
+  goToQuestions(level, points) {
+    if (this.points >= points) {
+      this.navCtrl.push(QuestionPage);
+    } else {
+      this.showAlert(`Vc precisa de pelos menos ${points} XP para ter acesso a essas questões`);
+    }
   }
+
+  showAlert(msg) {
+    const alert = this.alertCtrl.create({
+      title: 'Ops! Consquite mais XPs',
+      subTitle: msg,
+      buttons: ['Entendi']
+    });
+    alert.present();
+  }
+
 
 }
